3bd27daf5578cfc4b44472c54a7bffba7f414f51
thecrazyskull <anaskarbila@aospa.co>
2016-07-10 06:32:58 -0400

    BatteryService: Add Dash charging support
    Change-Id: I0515a3b6c643936a14b764530ffa58396beaadb8
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -67,9 +67,12 @@ import com.android.server.health.HealthServiceWrapper;
 import com.android.server.lights.LightsManager;
 import com.android.server.lights.LogicalLight;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayDeque;
@@ -167,6 +170,9 @@ public final class BatteryService extends SystemService {
 
     private boolean mOemFastCharger;
     private boolean mLastOemFastCharger;
+    private boolean mDashCharger;
+    private boolean mHasDashCharger;
+    private boolean mLastDashCharger;
 
     private long mDischargeStartTime;
     private int mDischargeStartLevel;
@@ -202,6 +208,9 @@ public final class BatteryService extends SystemService {
         mLed = new Led(context, getLocalService(LightsManager.class));
         mBatteryStats = BatteryStatsService.getService();
         mActivityManagerInternal = LocalServices.getService(ActivityManagerInternal.class);
+
+        mHasDashCharger = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_hasDashCharger);
 
         mCriticalBatteryLevel = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_criticalBatteryWarningLevel);
@@ -535,6 +544,7 @@ public final class BatteryService extends SystemService {
         shutdownIfOverTempLocked();
 
         mOemFastCharger = isOemFastCharger();
+        mDashCharger = mHasDashCharger && isDashCharger();
 
         if (force
                 || (mHealthInfo.batteryStatus != mLastBatteryStatus
@@ -548,7 +558,8 @@ public final class BatteryService extends SystemService {
                         || mHealthInfo.maxChargingVoltageMicrovolts != mLastMaxChargingVoltage
                         || mHealthInfo.batteryChargeCounterUah != mLastChargeCounter
                         || mInvalidCharger != mLastInvalidCharger
-                        || mOemFastCharger != mLastOemFastCharger)) {
+                        || mOemFastCharger != mLastOemFastCharger
+                        || mDashCharger != mLastDashCharger)) {
 
             if (mPlugType != mLastPlugType) {
                 if (mLastPlugType == BATTERY_PLUGGED_NONE) {
@@ -723,6 +734,7 @@ public final class BatteryService extends SystemService {
             mLastBatteryLevelCritical = mBatteryLevelCritical;
             mLastInvalidCharger = mInvalidCharger;
             mLastOemFastCharger = mOemFastCharger;
+            mLastDashCharger = mDashCharger;
         }
     }
 
@@ -755,6 +767,7 @@ public final class BatteryService extends SystemService {
                 mHealthInfo.maxChargingVoltageMicrovolts);
         intent.putExtra(BatteryManager.EXTRA_CHARGE_COUNTER, mHealthInfo.batteryChargeCounterUah);
         intent.putExtra(BatteryManager.EXTRA_OEM_FAST_CHARGER, mOemFastCharger);
+        intent.putExtra(BatteryManager.EXTRA_DASH_CHARGER, mDashCharger);
         if (DEBUG) {
             Slog.d(TAG, "Sending ACTION_BATTERY_CHANGED. scale:" + BATTERY_SCALE
                     + ", info:" + mHealthInfo.toString()
@@ -825,7 +838,20 @@ public final class BatteryService extends SystemService {
         } catch (IOException e) {
             Slog.e(TAG, "Failed to read oem fast charger status path: " + path);
         }
-
+        return false;
+    }
+    
+    private boolean isDashCharger() {
+        try {
+            FileReader file = new FileReader("/sys/class/power_supply/battery/fastchg_status");
+            BufferedReader br = new BufferedReader(file);
+            String state = br.readLine();
+            br.close();
+            file.close();
+            return "1".equals(state);
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
         return false;
     }
 
