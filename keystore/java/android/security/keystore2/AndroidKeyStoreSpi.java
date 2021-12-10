4c26276aba1b1706a0961112842cf3d6a971f93c
Anay Wadhera <awadhera@berkeley.edu>
2021-12-10 13:11:49 -0800

    PixelPropsUtils: Remove spoofing for currently supported Pixel devices
    @neobuddy89: Selectively enabled for GPhotos spoof.
    
    Change-Id: I3a426f8671f841c16e5af7c0a2a204d19c502464
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -77,7 +77,7 @@ import java.util.Set;
 
 import javax.crypto.SecretKey;
 
-import com.android.internal.util.crdroid.PixelPropsUtils;
+import com.android.internal.util.catalyst.PixelPropsUtils;
 
 /**
  * A java.security.KeyStore interface for the Android KeyStore. An instance of
