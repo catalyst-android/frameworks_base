crdroidandroid/android_frameworks_base

Introduce PixelPropsUtils for safety net spoof
* That will spoof build fingerprints on some g00gle apps
* Thanks to kdrag0n for the original idea at
  https://github.com/ProtonAOSP/android_frameworks_base/commit/5a54bfd846c7a26ba4820a361a6fec779edf8c5a

@neobuddy89:
* Squash subsequent changes by jhenrique09, SKULSHADY, Stallix

Signed-off-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
Co-authored-by: Danny Lin <danny@kdrag0n.dev>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/com/android/internal/os/Zygote.java
@@ -35,6 +35,8 @@ import android.system.ErrnoException;
 import android.system.Os;
 import android.util.Log;
 
+import com.android.internal.util.crdroid.PixelPropsUtils;
+
 import dalvik.annotation.optimization.FastNative;
 import dalvik.system.ZygoteHooks;
 
@@ -794,6 +796,9 @@ public final class Zygote {
         } else {
             Log.w(loggingTag, "Unable to set package name.");
         }
+
+        // Set pixel props
+        PixelPropsUtils.setProps(args.mPackageName);
     }
 
     private static final String USAP_ERROR_PREFIX = "Invalid command to USAP: ";

catalyst-android/frameworks_base

Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

core/java/com/android/internal/os/Zygote.java
@@ -36,6 +36,8 @@ import android.util.Log;
 
 import com.android.internal.net.NetworkUtilsInternal;
 
+import com.android.internal.util.catalyst.PixelPropsUtils;
+
 import dalvik.annotation.optimization.CriticalNative;
 import dalvik.annotation.optimization.FastNative;
 import dalvik.system.ZygoteHooks;
@@ -885,6 +887,9 @@ public final class Zygote {
         } else {
             Log.w(loggingTag, "Unable to set package name.");
         }
+
+        // Set pixel props
+        PixelPropsUtils.setProps(args.mPackageName);
     }
 
     private static final String USAP_ERROR_PREFIX = "Invalid command to USAP: ";